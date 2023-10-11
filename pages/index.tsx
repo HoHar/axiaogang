082d0188c6702475875e0b642d6919a8e98a4e8d
Howard <me@hohar.top>
2023-10-11 09:24:24 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -735,7 +735,7 @@ const Home: React.FC<HomeProps> = ({
   return (
     <>
       <Head>
-        <title>GPT3.5 - 一程山路</title>
+        <title>ChatGPT</title>
         <meta name="description" content="" />
         <meta
           name="viewport"
